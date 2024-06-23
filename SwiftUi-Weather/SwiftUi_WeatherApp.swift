5f740090cce6e911e6a62d8d262328ffbce1eac0
Omid Zaheri <omid.zaheri380@gmail.com>
2024-06-23 11:10:11 +0330

    Initial Commit

---
+++ SwiftUi-Weather/SwiftUi_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_WeatherApp.swift
+//  SwiftUi-Weather
+//
+//  Created by Omid Zaheri on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
